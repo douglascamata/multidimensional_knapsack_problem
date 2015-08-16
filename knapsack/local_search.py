fcf969e59cf8e44710255786df1e1a60d78a665b
Douglas Camata <d.camata@gmail.com>
2015-08-16 02:07:46 -0300

    added some comments about the local search functions implementations

--- knapsack/local_search.py
+++ knapsack/local_search.py
@@ -1,3 +1,7 @@
+# Both local search functions in this file would need to be updated to actually
+# execute movements in the knapsack and find a better solution. See the Tabu
+# Search implementation for an idea of how to do this.
+#
 def best_improving(solutions, knapsack):
     sorted_solutions = sorted(solutions, key=lambda move: move.movement_avaliation, reverse=True)
     if len(sorted_solutions) == 0:
